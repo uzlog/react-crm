ffcca0bbf5f06ef3dbd1d52adb250d8b11f4fc4e
Colton Travers <contactcolt@gmail.com>
2020-03-03 20:53:30 -0500

    Update and flush out useGet some more.

--- src/api/hooks/useGet.js
+++ src/api/hooks/useGet.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
+import fetch from "isomorphic-fetch";
 import isEqual from "lodash/isEqual";
 import buildUrlWithUrlParams from "../helpers/buildUrlWithUrlParams";
 import usePreviousProp from "../../hooks/usePreviousProp";
 
 export const useGet = ({ url, urlParams = {}, requestOptions = {} } = {}) => {
     const [isLoading, setIsLoading] = useState(false);
-    const [response, setResponse] = useState(false); // will be overwritten with response object
+    const [response, setResponse] = useState(false);
     const [error, setError] = useState(false);
     const previousUrl = usePreviousProp(url);
     const previousUrlParams = usePreviousProp(urlParams);
@@ -19,22 +20,33 @@ export const useGet = ({ url, urlParams = {}, requestOptions = {} } = {}) => {
 
     useEffect(() => {
         const fetchData = async () => {
-            console.log("Making call!");
             setIsLoading(true);
 
             try {
-                // eslint-disable-next-line no-undef
                 const data = await fetch(urlString, requestOptions);
-                const json = await data.json();
-                console.log({ json });
 
-                setResponse(json);
+                if (!data.ok) {
+                    throw data;
+                }
+
+                const parsedData = await data.json();
+                setResponse(parsedData);
             } catch (err) {
-                console.log({ err });
-                setError(true);
+                setIsLoading(false);
+
+                if (err instanceof Error) {
+                    return setError({ message: err.message });
+                }
+
+                const { statusCode, message } = await err.json();
+
+                return setError({
+                    statusCode,
+                    message
+                });
             }
 
-            setIsLoading(false);
+            return setIsLoading(false);
         };
 
         if (
